e40ea342f56656c0f2683f9dd863260f2b677099
Nuno Correia <nunojscorreia@hotmail.com>
2022-07-17 11:09:30 -0300

    Adding product page content with product category

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  productCategory: String = "";
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.productCategory = params['cat'];
+      console.log(this.productCategory);
+    });
   }
 
+  getHeaderImage(): string {
+    const imagePath = "../../../assets/products/";
+    switch(this.productCategory) { 
+      case 'registers': { 
+         return imagePath + "register.jpg"
+      } 
+      case 'pos': { 
+        return imagePath + "POS2.jpg" 
+      } 
+      case 'slicers': { 
+        return imagePath + "slicer.jpg" 
+     } 
+      case 'phones': { 
+        return imagePath + "phone.jpg" 
+      } 
+      case 'scales': { 
+        return imagePath + "scale.jpg" 
+      } 
+      case 'security-systems': { 
+        return imagePath + "security.jpg" 
+      } 
+      default: { 
+        return "" 
+      } 
+    } 
+  }
 }
